1da5b00debe929986c418e9f85ebc53412495e3e
Sascha Depold <sascha@depold.com>
2012-12-16 22:17:24 +0100

    first steps

--- rsvm.rs
+++ rsvm.rs
@@ -1,13 +1,15 @@
 const VERSION: &static/str = "0.0.1";
 
 fn main() {
-    match os::args()[1] {
+    let command = if os::args().len() == 1 { ~"" } else { os::args()[1] };
+
+    match command {
       ~"--version" | ~"-v" => {
         io::println(~"rsvm " + VERSION)
       }
 
       _ => {
-        print_teaser()
+        print_help()
       }
     }
 }
@@ -17,4 +19,18 @@ fn print_teaser() {
     io::println("Rust Version Manager");
     io::println("====================");
     io::println("");
+}
+
+fn print_help() {
+    print_teaser();
+
+    io::println("Usage:");
+    io::println("");
+    io::println("  rsvm help | --help | -h       Show this message.");
+    io::println("  rsvm install <version>        Download and install a <version>. <version> could be for example '0.4'.");
+    // echo '  rsvm uninstall <version>      Uninstall a <version>.'
+    io::println("  rsvm use <version>            Activate <version> for now and the future.");
+    io::println("  rsvm ls | list                List all installed versions of rust.");
+    io::println("");
+    io::println(~"Current version: " + VERSION);
 }
