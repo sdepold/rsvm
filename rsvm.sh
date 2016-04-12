066eeb7e5e607aaf549b377c128a50caa03032b8
Sunguk Lee <d3m3vilurr@gmail.com>
2016-04-13 06:47:04 +0900

    rsvm.sh: Improve migrate function
    `rsvm_initailize` will use version data;
    
    ```
    if [ "$rsvm_version" = "1" ]
    then
      // something
    elif [ "$rsvm_version" = "2" ]
    then
      // next
    fi
    echo $last_version > "$RSVM_DIR/.rsvm_version"
    ```

--- rsvm.sh
+++ rsvm.sh
@@ -33,28 +33,28 @@ fi
 
 rsvm_initialize()
 {
-  if [ -d "$RSVM_DIR/versions" ]
-  then
-    return
-  elif [ ! -d "$RSVM_DIR" ]
+  if [ ! -d "$RSVM_DIR/versions" ]
   then
     mkdir -p "$RSVM_DIR/versions"
-    return
   fi
-
-  DIRECTORIES=$(find "$RSVM_DIR" -maxdepth 1 -mindepth 1 -type d -exec basename '{}' \; \
-    | sort \
-    | egrep "^$RSVM_VERSION_PATTERN")
-
-  mkdir -p "$RSVM_DIR/versions"
-  if [ $(egrep -o "^$RSVM_VERSION_PATTERN" <<< "$DIRECTORIES" | wc -l) = 0 ]
+  if [ ! -f "$RSVM_DIR/.rsvm_version" ]
   then
-    return
+    touch "$RSVM_DIR/.rsvm_version"
   fi
-  for line in $(echo $DIRECTORIES | tr " " "\n")
-  do
-    mv "$RSVM_DIR/$line" "$RSVM_DIR/versions"
-  done
+  local rsvm_version=$(cat "$RSVM_DIR/.rsvm_version")
+  if [ -z "$rsvm_version" ]
+  then
+    local DIRECTORIES=$(find "$RSVM_DIR" -maxdepth 1 -mindepth 1 -type d -exec basename '{}' \; \
+      | sort \
+      | egrep "^$RSVM_VERSION_PATTERN")
+
+    mkdir -p "$RSVM_DIR/versions"
+    for line in $(echo $DIRECTORIES | tr " " "\n")
+    do
+      mv "$RSVM_DIR/$line" "$RSVM_DIR/versions"
+    done
+  fi
+  echo "1" > "$RSVM_DIR/.rsvm_version"
 }
 
 
