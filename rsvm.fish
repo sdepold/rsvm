953f8368441102828197e715bc3c0bc4f5ff476a
Sunguk Lee <d3m3vilurr@gmail.com>
2017-05-28 23:59:13 +0900

    Fix cargo path on the fish environ

--- rsvm.fish
+++ rsvm.fish
@@ -34,8 +34,9 @@ function rsvm_set_path
     end
   end
 
-  set p (rsvm_find_paths $r | head -n1)
-  set newpath $p $newpath
+  for p in (rsvm_find_paths $r)
+    set newpath $p $newpath
+  end
   rsvm_set $k $newpath
 end
 
